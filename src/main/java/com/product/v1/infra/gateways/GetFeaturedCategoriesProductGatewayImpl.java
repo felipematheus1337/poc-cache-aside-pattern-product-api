d8b308c91776e52444f17e4f5c5e72007761fa94
felipematheus1337 <lipehbr@gmail.com>
2026-03-16 15:02:02 -0300

    feature: finished the business logic for searching for the products with featured categories.

---
+++ src/main/java/com/product/v1/infra/gateways/GetFeaturedCategoriesProductGatewayImpl.java
@@ -0,0 +1,24 @@
+package com.product.v1.infra.gateways;
+
+import com.product.v1.application.gateways.GetProductsAndCategoriesFeaturedsGateway;
+import com.product.v1.application.usecases.getproductsandcategories.FindProductsAndCategoriesFeaturedOutput;
+import com.product.v1.infra.persistence.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class GetFeaturedCategoriesProductGatewayImpl implements GetProductsAndCategoriesFeaturedsGateway {
+
+    private final ProductRepository repository;
+
+    @Override
+    public List<FindProductsAndCategoriesFeaturedOutput> execute() {
+        log.info("::: Searching for the products with the categories featured.");
+        return repository.getFeaturedProductsAndCategories();
+    }
+}
